4f216468e60444c18202f83ad48acf6675eda0e8
Mark Stiles <stiles.mark@gmail.com>
2014-03-19 15:19:28 -0400

    adding NUnitTesting.UnitTests library

---
+++ Libraries/NUnitTesting.UnitTests/Tests/SampleTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTesting.UnitTests.Tests {
+	[TestFixture, Category("Sample Tests")]
+	public class SampleTests {
+
+		[SetUp]
+		public void SetUp() { }
+
+		[Test]
+		public void Sample_TrueTest() {
+			Assert.IsTrue(true);
+		}
+	}
+}
